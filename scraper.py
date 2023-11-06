e065f74ee0dd8db9a39e758d41e37924ba122595
Shanazar <snizamaddinow@gmail.com>
2023-11-06 17:46:43 +0300

    feature: get table headers.

--- scraper.py
+++ scraper.py
@@ -6,19 +6,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from time import sleep
+from bs4 import BeautifulSoup
 
 base_url = 'https://yokatlas.yok.gov.tr/tercih-sihirbazi-t4-tablo.php?p=say'
 
 def get_options():
     options = webdriver.ChromeOptions()
-    # Add the desired headers
+    # options.add_argument('--headless') 
     headers = {
         "Accept": "application/json, text/javascript, */*; q=0.01",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9,tr;q=0.8,ru;q=0.7",
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "Cookie": "_ga=GA1.3.707879593.1699213628; _gid=GA1.3.1976060829.1699213628; _gat=1; _ga_V5NGP2K979=GS1.3.1699213628.1.0.1699213628.0.0.0",
-        # Add other headers here
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest",
         "sec-ch-ua": '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
@@ -31,12 +31,42 @@ def get_options():
 
     return options
 
+def get_table_header(table_header):
+    print("HEADER CALLED")
+    if table_header:
+        header_cells = table_header.find_all('th')
+        headers = []
+        for cell in header_cells:
+            cell_text = ''.join(cell.find_all(string=True, recursive=False)).strip().rstrip('*')
+
+            if cell_text:
+                headers.append(cell_text)
+        return headers
+
+def scrape_page(driver, headers):
+    try:
+        table = driver.find_element(By.ID, 'mydata')
+        outer_html = table.get_attribute('outerHTML')
+        soup = BeautifulSoup(outer_html, 'html.parser')
+
+        if not headers:
+            table_head = soup.find('thead')
+            headers.extend(get_table_header(table_head))
+
+        print(headers)
+
+    except:
+        print('Error while scraping page')
+    
+
 def main():
+    headers = []
     service = Service()
     options = get_options()
     driver = webdriver.Chrome(service=service, options=options)
     driver.get(base_url)
     timeout = 5
+
 
     try:
         element_present = EC.presence_of_element_located((By.ID, 'mydata_processing'))
@@ -49,22 +79,24 @@ def main():
         
         # Select 100 entries per page
         select.select_by_value('100')
-        print("Selected 100 entries per page")
         sleep(1)
-        
-        next_button = driver.find_element(By.ID, 'mydata_next')
+        count = 0
+        while (count := count + 1) < 10:
+            scrape_page(driver, headers)
 
-        if 'disabled' not in next_button.get_attribute('class'):
-            next_button.click()
-            print("Clicked on 'Sonraki' for the next page")
-        else:
-            print("Already on the last page. 'Sonraki' button is disabled.")
+            next_button = driver.find_element(By.ID, 'mydata_next')
+
+            if 'disabled' in next_button.get_attribute('class'):
+                break
+            else:
+                next_button.click()
+                sleep(1)
+                print("Clicked on 'Sonraki' for the next page")
 
     except TimeoutException:
         print("Timed out waiting for page to load")
     finally:
-        print("Page loaded")
-        sleep(10)
+        sleep(4)
         driver.quit()
 
 
